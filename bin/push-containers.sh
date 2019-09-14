9816baacdde61ba233ffd8e485d5973f0c2f7a98
Alexander Tsybulsky <atsybulsky@sbcg.com.ua>
2019-09-14 14:27:27 +0000

    improve release procedure

--- bin/push-containers.sh
+++ bin/push-containers.sh
@@ -8,6 +8,21 @@ fi
 
 echo "abaplint version:" $VERSION
 
+VERSION_2S=${VERSION%.*}
+VERSION_1S=${VERSION_2S%.*}
+
+echo "additional tags: $VERSION_2S, $VERSION_1S, latest"
+
 docker tag abaplint/abaplint abaplint/abaplint:$VERSION
+docker tag abaplint/abaplint abaplint/abaplint:$VERSION_2S
+docker tag abaplint/abaplint abaplint/abaplint:$VERSION_1S
+
 docker push abaplint/abaplint:latest
 docker push abaplint/abaplint:$VERSION
+docker push abaplint/abaplint:$VERSION_2S
+docker push abaplint/abaplint:$VERSION_1S
+
+#cleanup
+docker rmi abaplint/abaplint:$VERSION
+docker rmi abaplint/abaplint:$VERSION_2S
+docker rmi abaplint/abaplint:$VERSION_1S
